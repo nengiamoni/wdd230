64804a0b7ae07d56851b5bf1bac634373c021f59
nengiamoni <amonifavour46@gmail.com>
2024-05-03 23:40:42 +0100

    social media meta

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,8 +1,22 @@
-// Get the current year and update the footer
-const yearSpan = document.getElementById("year");
-const currentYear = new Date().getFullYear();
-yearSpan.textContent = currentYear;
+document.addEventListener("DOMContentLoaded", function () {
+    let yearSpan = document.getElementById("year");
+    let lastModifiedParagraph = document.getElementById("lastModified");
 
-// Get the last modified date and update the second paragraph in the footer
-const lastModifiedParagraph = document.getElementById("lastModified");
-lastModifiedParagraph.textContent = "Last modified: " + document.lastModified;
+    let ultimaModificacion = new Date(document.lastModified);
+    let options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        timeZoneName: "short",
+    };
+    let fechaFormato = ultimaModificacion.toLocaleString("en-US", options);
+
+    lastModifiedParagraph.textContent = "Last Modified: " + fechaFormato;
+
+    let year = new Date().getFullYear();
+    yearSpan.textContent = year;
+});
